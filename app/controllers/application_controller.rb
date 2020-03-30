0c6dc63114c0a507c6ca8b74ca2d8378f547453e
RafaS92 <manrvaldez..92@gmail.com>
2020-03-30 17:40:59 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 class ApplicationController < Sinatra::Base
 
     set(:views, 'app/views')
-    
 
+    get "/list_airlines" do
+        @airlines = Airline.all
+        
+        erb :list_airlines
+    end
+    
+    get "/get-airline/:id" do 
+        @single_airline = Airline.find(params[:id])
+       
+       
+        
+            
+
+        erb :single_airline
+    end
+ 
 end
 
 
